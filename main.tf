12acd83e63d050dc68f783ab606be0d7ab0e2509
Adisasson21 <96379294+Adisasson21@users.noreply.github.com>
2022-07-17 17:29:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "${var.region}"
 }
-<p> נציין איפה נרצה שהתשתית שלנו תפעל </p>
+נציין איפה נרצה שהתשתית שלנו תפעל
 
 resource "aws_vpc" "vpc" {
   cidr_block       = "${var.vpc-cidr}"  # variable of the vpc ip
@@ -13,7 +13,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-<p> הענן הפרטי מאפשר לנו לבודד את הענן בין לקוחות שונים  </p>
+הענן הפרטי מאפשר לנו לבודד את הענן בין לקוחות שונים
 
 resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = aws_vpc.vpc.id
@@ -24,8 +24,8 @@ resource "aws_internet_gateway" "internet-gateway" {
 
 
 # Route table for Public Subnets
-<p> ניתוב המידע,  היא בעצם אחראית לאן תופנה תעבורת הרשת מהתת רשת </p>
-<p> הניתוב הזה הוא קישור של המידע המועבר מהנתב לרשת החיצונית (האינטרנט) </p>
+ניתוב המידע,  היא בעצם אחראית לאן תופנה תעבורת הרשת מהתת רשת
+הניתוב הזה הוא קישור של המידע המועבר מהנתב לרשת החיצונית (האינטרנט)
   
 resource "aws_route_table" "public-route-table" {   
   vpc_id =  aws_vpc.vpc.id
@@ -41,11 +41,11 @@ resource "aws_route_table" "public-route-table" {
 
   
 #Subnet
-<p> היא חלק מרשת גדולה, היא נועדה לעזור לזרימת תעבורת הרשת להיות יותר יעילה </p>
+היא חלק מרשת גדולה, היא נועדה לעזור לזרימת תעבורת הרשת להיות יותר יעילה
   
 # Public Subnet
-<p> היא רשת משנה שמאפשרת למופעים ברשת להיות נגישים לאינטרנט ואנשים יש גישה לאותם מופעים דרך האינטרנט </p>
-<p>  נבחר ברשת משנה כזאת כאשר מופעים חייבים להיות בתקשורת עם האינטרנט לדוגמא: בלוג </p>
+היא רשת משנה שמאפשרת למופעים ברשת להיות נגישים לאינטרנט ואנשים יש גישה לאותם מופעים דרך האינטרנט
+נבחר ברשת משנה כזאת כאשר מופעים חייבים להיות בתקשורת עם האינטרנט לדוגמא: בלוג
 
 resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.vpc.id
@@ -65,7 +65,7 @@ resource "aws_subnet" "public-subnet-2" {
 
 
 # Private Subnet
-<p>רשת משנה פרטים נועדה כדי לאבטח מופעי קצה שאינם צריכים להיות מחוברים לאינטרנט </p>
+רשת משנה פרטים נועדה כדי לאבטח מופעי קצה שאינם צריכים להיות מחוברים לאינטרנט
 
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                   = aws_vpc.vpc.id
@@ -85,8 +85,8 @@ resource "aws_subnet" "private-subnet-2" {
 }
 
 #Route table Association with Public Subnets
-<p> ניתב המידע מהתת רשת אל הנתב, לנתב אחד יכולים להיות כמה רשתות משנה שמחוברות אליו אבל רשת משנה לא יכולה להיות מחוברת לכמה נתבים שונים</p>
-<p> שני הרשתות משנה הציבוריות שיצרתי מחוברת לאותו נתב</p>
+ניתב המידע מהתת רשת אל הנתב, לנתב אחד יכולים להיות כמה רשתות משנה שמחוברות אליו אבל רשת משנה לא יכולה להיות מחוברת לכמה נתבים שונים
+שני הרשתות משנה הציבוריות שיצרתי מחוברת לאותו נתב
 resource "aws_route_table_association" "public-subnet-1-route-table-association" {
   subnet_id = aws_subnet.public-subnet-1.id
   route_table_id = aws_route_table.public-route-table.id
