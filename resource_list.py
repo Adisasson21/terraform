c179f4c1319cac3af2c84d3f4372dfc8ad23312e
Adisasson21 <96379294+Adisasson21@users.noreply.github.com>
2022-07-19 18:50:38 +0300

    Update resource_list.py

--- resource_list.py
+++ resource_list.py
@@ -4,7 +4,7 @@ session = boto3.Session(region_name='us-east-1')
 client = session.client(service_name='ec2')
 all_regions = client.describe_regions()
 
-region_list = ['us-east-1', 'us-east-2']
+region_list = ['us-east-1', ]
 list_of_regions = []
 for each_reg in all_regions['Regions']:
     list_of_regions.append(each_reg['RegionName'])
@@ -16,10 +16,14 @@ for each_reg in list_of_regions:
     if each_reg in region_list:
         print("List of services: ", each_reg)
         for each_in_vpc in resource.vpcs.all():
-            print(each_in_vpc.id)
+            print(client.describe_vpcs(VpcIds=[each_in_vpc.id]))
+        print('-------------------------------------------------------------------')
         for each_in_subnet in resource.subnets.all():
-            print(each_in_subnet.id)
+            print(client.describe_subnets(SubnetIds=[each_in_subnet.id]))
+        print('-------------------------------------------------------------------')
         for each_in_IGW in resource.internet_gateways.all():
-            print(each_in_IGW.id)
+            print(client.describe_internet_gateways(InternetGatewayIds=[each_in_IGW.id]))
+        print('-------------------------------------------------------------------')
         for each_in_sg in resource.security_groups.all():
-            print(each_in_sg.id)
+            print(client.describe_security_groups(GroupIds=[each_in_sg.id]))
+
